6feb8c82a8b773e048efa8fc5c9efc2de3b8ccd7
Alexandria <walkerciara24@gmail.com>
2024-10-31 12:53:49 -0400

    Update app.js

--- app.js
+++ app.js
@@ -4,3 +4,19 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+// View engine setup
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+// Basic route for testing
+app.get('/', (req, res) => {
+    res.render('index', { 
+        title: 'Little Recipe Book',
+        message: 'Welcome to the Recipe Book!'
+    });
+});
